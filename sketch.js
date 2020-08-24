1a7f972943ce477b398929d9e9b56f63cacaa532
Krishnanischal <krishnanischal06@gmail.com>
2020-08-24 18:55:41 +0530

    animation

--- sketch.js
+++ sketch.js
@@ -9,7 +9,18 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1_img,car2_img,car3_img,car4_img;
+var ground_img,track;
 
+function perload(){
+car1_img=loadImage("../images/car1.png");
+car2_img=loadImage("../images/car2.png");
+car3_img=loadImage("../images/car3.png");
+car4_img=loadImage("../images/car4.png");
+ground_img=loadImage("../images/ground.png");
+track=loadImage("../images/track.jpg");
+
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
